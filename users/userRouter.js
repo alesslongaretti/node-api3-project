80bc7b5c5acb06808a77373741a4214d28227a5f
alesslongaretti <alesslongaretti@gmail.com>
2020-03-19 20:42:24 -0400

    got all Crud operations to work for users, working on the Post for the posts

--- users/userRouter.js
+++ users/userRouter.js
@@ -4,32 +4,89 @@ const User = require('./userDb.js');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+// WORKING
+router.post('/',validateUser, (req, res) => {
   // do your magic!
+  User.insert(req.body)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error adding user", err})
+    })
 });
 
+
+//
 router.post('/:id/posts', (req, res) => {
   // do your magic!
 });
 
+
+// WORKING
 router.get('/', (req, res) => {
   // do your magic!
+  User.get(req.query)
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "error retrieving the users", err})
+    })
 });
 
-router.get('/:id', (req, res) => {
+
+// WORKING
+router.get('/:id', validateUserId, (req, res) => {
   // do your magic!
+  res.status(200).json(req.user)
 });
 
-router.get('/:id/posts', (req, res) => {
+
+// WORKING
+router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
+  User.getUserPosts(req.params.id)
+    .then(posts => {
+      res.status(200).json({ posts})
+    })
+    .catch( err => {
+      res.status(500).json({ message: "Error getting the posts of the user", err})
+    })
 });
 
-router.delete('/:id', (req, res) => {
+
+// WORKING
+router.delete('/:id', validateUserId, (req, res) => {
   // do your magic!
+  User.remove(req.params.id)
+    .then(count => {
+      if(count > 0) {
+        res.status(200).json({ message: "The user has been deleted"});
+      } else {
+        res.status(400).json({ message: "The user could not be found"})
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error removing the user", err})
+    })
 });
 
-router.put('/:id', (req, res) => {
+
+//
+router.put('/:id', [validateUser, validateUserId], (req, res) => {
   // do your magic!
+  User.update(req.params.id, req.body)
+    .then(user =>  {
+      if(user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: "The user could not be found"})
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Error updating the user", err})
+    })
 });
 
 //custom middleware
@@ -57,9 +114,9 @@ function validateUser(req, res, next) {
   body = req.body;
   if(!body && body === {}) {
     res.status(400).json({ message: "missing user data" })
-    next();
   }else if (!body.name) {
     res.status(400).json({ message: "missing required name field"});
+  } else {
     next();
   }
 }
